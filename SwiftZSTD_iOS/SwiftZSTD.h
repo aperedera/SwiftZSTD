b27175b55663dda1c85b2c35b2f2897b346be3db
Clément Troesch <clement.troesch@gmail.com>
2019-03-22 15:29:39 +0100

    Share iOS and macOS schemes for Carthage support

--- SwiftZSTD_iOS/SwiftZSTD.h
+++ SwiftZSTD_iOS/SwiftZSTD.h
@@ -16,8 +16,8 @@ FOUNDATION_EXPORT const unsigned char SwiftZSTDVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftZSTD_iOS/PublicHeader.h>
 
-#import <zstd.h>
-#import <zdict.h>
+#import "zstd.h"
+#import "zdict.h"
 
-#import <StreamHelpers.h>
+#import "StreamHelpers.h"
 
